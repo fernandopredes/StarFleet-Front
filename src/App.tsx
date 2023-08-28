524c2b2cc263997f5e9011297571ad5dee670c3f
fernando predes <fernando_predes@hotmail.com>
2023-08-27 21:01:29 -0300

    added spacejump effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
+import { useState } from 'react';
 import './App.css'
+import SpaceJump from './components/SpaceJump'
 import LoginPage from './pages/LoginPage'
 
 function App() {
 
+  const [, setIsLogged] = useState(false);
+  const [showSpaceJump, setShowSpaceJump] = useState(false);
+
+  const handleLogin = () => {
+    setIsLogged(true);
+    setShowSpaceJump(true);
+
+    setTimeout(() => {
+      setShowSpaceJump(false);
+    }, 8000); // Faz o SpaceJump desaparecer depois de 8 segundos
+  };
 
   return (
     <>
-      <LoginPage/>
+      <LoginPage onLogin={handleLogin}/>
+      {showSpaceJump && <SpaceJump />}
     </>
   )
 }
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import video from '../assets/video.mp4';
 import LoginForm from '../components/LoginForm';
+import { useSpring, animated } from 'react-spring';
+import bg from '../assets/full.jpg'
 
-const VideoContainer = styled.div`
+const Container = styled.div`
+  background-image: url(${bg});
+  background-size:     cover;
+  background-repeat:   no-repeat;
+  background-position: center center;
+  overflow: hidden;
+  position: relative;
+  height: 100vh;
+`;
+
+const VideoContainer = styled(animated.div)`
   width: 50vw;
   height: 100vh;
   overflow: hidden;
@@ -18,34 +30,70 @@ const VideoContainer = styled.div`
   }
 `;
 
-const LoginContainer = styled.div`
+const LoginContainer = styled(animated.div)`
   width: 50vw;
   height: 100vh;
   position: absolute;
   right: 0;
   top: 0;
-  background-color: #010101;
   color: #FEF5EF;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-const LoginPage: React.FC = () => {
+const LoginPage: React.FC<{ onLogin: () => void }> = ({ onLogin }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [, setShowSpaceJump] = useState(false)
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      setShowSpaceJump(true); // Iniciar a animação SpaceJump
+
+      // Esconder a animação após 8 segundos
+      const timeout = setTimeout(() => {
+        setShowSpaceJump(false);
+      }, 8000);
+
+      // Limpar o timeout quando o componente for desmontado
+      return () => clearTimeout(timeout);
+    }
+  }, [isLoggedIn]);
+
+  // Animação para deslizar o vídeo para a esquerda
+  const videoAnimationProps = useSpring({
+    transform: isLoggedIn ? 'translateX(-100%)' : 'translateX(0%)',
+    config: {
+      tension: 100,
+      friction: 60
+    }
+  });
+
+  // Animação para deslizar o container de login para a direita
+  const loginAnimationProps = useSpring({
+    transform: isLoggedIn ? 'translateX(100%)' : 'translateX(0%)',
+    config: {
+      tension: 100,
+      friction: 60
+    }
+  });
+
   return (
-    <div>
-      <VideoContainer>
+    <Container>
+      <VideoContainer style={videoAnimationProps}>
         <video autoPlay loop muted>
           <source src={video} type="video/mp4" />
         </video>
       </VideoContainer>
-      <LoginContainer>
+      <LoginContainer style={loginAnimationProps}>
         <LoginForm onSubmit={(email, password) => {
             console.log(`Trying to login with ${email} and ${password}`);
+            setIsLoggedIn(true); // For demo purposes, setting the state to true here
+            onLogin()
           }}
         />
       </LoginContainer>
-    </div>
+    </Container>
   );
 };
 
