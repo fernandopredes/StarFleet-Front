bb70a23f4ed08a1dd4348c20916767383f7afe5d
fernando predes <fernando_predes@hotmail.com>
2023-09-03 19:31:54 -0300

    added frame

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -4,6 +4,7 @@ import video from '../assets/video.mp4';
 import LoginForm from '../components/LoginForm';
 import { useSpring, animated } from 'react-spring';
 import bg from '../assets/full.jpg'
+import frame from '../assets/frame.png'
 
 const Container = styled.div`
   background-image: url(${bg});
@@ -22,10 +23,17 @@ const VideoContainer = styled(animated.div)`
   position: absolute;
   left: 0;
   top: 0;
+  clip-path: circle(25vw at center);
 
   video {
-    width: 100%;
-    height: 100%;
+    width: 25vw;
+    height: 25vw;
+    max-height: 100vh;
+    max-width: 100vh;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
     object-fit: cover;
   }
 `;
@@ -42,25 +50,34 @@ const LoginContainer = styled(animated.div)`
   align-items: center;
 `;
 
+const CircularFrame = styled.div`
+  width: 50vw;
+  height: 50vw;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: url(${frame}) no-repeat center/cover;
+  border-radius: 50%;
+  z-index: 1; // para garantir que ele fique no topo do vídeo
+`;
+
 const LoginPage: React.FC<{ onLogin: () => void }> = ({ onLogin }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [, setShowSpaceJump] = useState(false)
 
   useEffect(() => {
     if (isLoggedIn) {
-      setShowSpaceJump(true); // Iniciar a animação SpaceJump
+      setShowSpaceJump(true);
 
-      // Esconder a animação após 8 segundos
       const timeout = setTimeout(() => {
         setShowSpaceJump(false);
       }, 8000);
 
-      // Limpar o timeout quando o componente for desmontado
       return () => clearTimeout(timeout);
     }
   }, [isLoggedIn]);
 
-  // Animação para deslizar o vídeo para a esquerda
   const videoAnimationProps = useSpring({
     transform: isLoggedIn ? 'translateX(-100%)' : 'translateX(0%)',
     config: {
@@ -84,6 +101,7 @@ const LoginPage: React.FC<{ onLogin: () => void }> = ({ onLogin }) => {
         <video autoPlay loop muted>
           <source src={video} type="video/mp4" />
         </video>
+        <CircularFrame />
       </VideoContainer>
       <LoginContainer style={loginAnimationProps}>
         <LoginForm onSubmit={(email, password) => {
